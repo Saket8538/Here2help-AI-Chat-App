834885d78d4ecccf917808bd08d3fab55550c438
Saket Kumar <126570621+Saket8538@users.noreply.github.com>
2024-04-10 10:33:59 +0530

    Create Index_Audio.js

---
+++ assets/js/Play Audio/Index_Audio.js
@@ -0,0 +1,28 @@
+var Audiotitleindiex = new Audio();
+Audiotitleindiex.src = "assets/audio/Index Title.wav";
+
+var AudioKeyterms = new Audio();
+AudioKeyterms.src = "assets/audio/key Values index.wav";
+
+var AudioVisionIndex = new Audio();
+AudioVisionIndex.src = "assets/audio/Vision Indiex.wav";
+
+var Audioinclusion = new Audio();
+Audioinclusion.src = "assets/audio/Fostering Inclusion.wav";
+
+function playAudiotitle()
+{
+    Audiotitleindiex.play();
+}
+function PlayAudioKeyterms()
+{
+    AudioKeyterms.play();
+}
+function PlayVisionIndex()
+{
+    AudioVisionIndex.play();
+}
+function PlayInclusion()
+{
+    Audioinclusion.play();
+}
